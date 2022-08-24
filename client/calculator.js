fa78999e912edddf4838360bd1e32d115b5d1398
colinmyers37 <colinmyers37@gmail.com>
2022-08-24 14:28:54 -0500

    added function for form sumbit but not working thinking it has to do with the server not delivering the javascript file right

---
+++ client/calculator.js
@@ -0,0 +1,13 @@
+let handicapIndex = document.querySelector('#handicap-index').value;
+let courseRating = document.querySelector('#course-rating').value;
+let slopeRating = document.querySelector('#slope-rating').value;
+let par = document.querySelector('#par').value;
+
+let form = document.querySelector('.calculator-form');
+
+
+form.addEventListener('submit', function(event){
+    event.preventDefault();
+    console.log(handicapIndex)
+    console.log(courseRating)
+})
