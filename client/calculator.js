4fef6a233e3b062f223e9246fed6c7f5660a577b
colinmyers37 <colinmyers37@gmail.com>
2022-08-24 14:45:01 -0500

    fixed problems accessing the values entered in the form

--- client/calculator.js
+++ client/calculator.js
@@ -1,15 +1,9 @@
-let handicapIndex = document.querySelector('#handicap-index').value;
-let courseRating = document.querySelector('#course-rating').value;
-let slopeRating = document.querySelector('#slope-rating').value;
-let par = document.querySelector('#par').value;
+let form = document.querySelector(".calculator-form");
 
-let form = document.querySelector('.calculator-form');
-
-
-console.log("hello world")
-
-form.addEventListener('submit', function(event){
-    event.preventDefault();
-    console.log(handicapIndex)
-    console.log(courseRating)
-})
+form.addEventListener("submit", function (event) {
+  event.preventDefault();
+  let handicapIndex = document.querySelector("#handicap-index").value;
+  let courseRating = document.querySelector("#course-rating").value;
+  let slopeRating = document.querySelector("#slope-rating").value;
+  let par = document.querySelector("#par").value;
+});
